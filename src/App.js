dd77a4388cf64d011f2ad1c113048f20f7b2f806
Cammy Smith <cjsarab1991@gmail.com>
2022-10-15 09:50:32 +0100

    Added most of functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, {useState, useEffect } from 'react';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    getItems();
+  }, [query])
+
+  const getItems = function(){
+    fetch(`https://hp-api.herokuapp.com/api/characters?name=${query}`)
+    .then(res => res.json())
+    .then(items => setItems(items));
+    setIsLoading(false)
+}
+
+
   return (
     <div className="container">
-      Hello
+      <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const CharacterItem = ({item}) => {
+  return (
+    <div className='card'>
+        <div className='card-inner'>
+            <div className='card-front'>
+                <img src={item.image} alt='' />
+            </div>
+            <div className='card-back'>
+                <h1>{item.name}</h1>
+                <ul>
+                    <li>
+                        <strong>Actor Name: </strong> {item.actor}
+                    </li>
+                    <li>
+                        <strong>Date of Birth: </strong> {item.dateOfBirth}
+                    </li>
+                    <li>
+                        <strong>Ancestry: </strong> {item.ancestry}
+                    </li>
+                    <li>
+                        <strong>Species: </strong> {item.species}
+                    </li>
+                    <li>
+                        <strong>House: </strong> {item.house}
+                    </li>
+                    <li>
+                        <strong>Patronus: </strong> {item.patronus}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default CharacterItem;
